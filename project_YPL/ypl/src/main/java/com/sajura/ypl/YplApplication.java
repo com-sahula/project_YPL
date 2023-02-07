ce4e955a08e92a674f545a4207513f787d069945
Hyun-Woo1230 <123930066+Hyun-Woo1230@users.noreply.github.com>
2023-02-07 20:19:55 +0900

    Add files via upload
    프로젝트 초기 설정 및 Review 기능

---
+++ project_YPL/ypl/src/main/java/com/sajura/ypl/YplApplication.java
@@ -0,0 +1,13 @@
+package com.sajura.ypl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YplApplication.class, args);
+	}
+
+}
---
+++ project_YPL/ypl/src/test/java/com/sajura/ypl/YplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sajura.ypl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
